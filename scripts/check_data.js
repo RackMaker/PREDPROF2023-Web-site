bf5b9b8643fd92875c5f9cc8de967adc11782136
RackMaker <72761969+RackMaker@users.noreply.github.com>
2023-03-17 22:15:43 +0300

    Delete scripts directory

--- scripts/check_data.js
+++
@@ -1,8 +0,0 @@
-function checking() {
-    login = document.querySelector('#login').value;
-    password = document.querySelector('#password').value;
-
-    eel.enter_to_account(login, password);
-
-    window.location.href = '../index.html';
-}
